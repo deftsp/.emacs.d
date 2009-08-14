c8da3431d3f180c61d379aea424063fb52b1cae2
S.P.Tseng <deftsp@gmail.com>
2009-08-14 13:14:13 +0800

    change some ediff behavior.

--- site-lisp/50ediff.el
+++ site-lisp/50ediff.el
@@ -5,6 +5,7 @@
 ;; Author: S.P.Tseng <deftsp@gmail.com>
 
 ;; reuse the selected frame
-(setq ediff-window-setup-function 'ediff-setup-windows-plain)
-(setq ediff-split-window-function 'split-window-vertically) ; split-window-horizontally
+(if window-system
+ (setq ediff-window-setup-function 'ediff-setup-windows-plain)) ; 'ediff-setup-windows-multiframe
+(setq ediff-split-window-function 'split-window-horizontally) ; 'split-window-vertically
 ;;; 50ediff.el ends here
