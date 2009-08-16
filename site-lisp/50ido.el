f347723330977e80daf298d873bb494bfb2e592d
S.P.Tseng <deftsp@gmail.com>
2009-08-16 10:21:37 +0800

    add ido-sort-mtime.

--- site-lisp/50ido.el
+++ site-lisp/50ido.el
@@ -164,6 +164,23 @@ directory, select directory. Lastly the file is opened."
 (add-hook 'ido-make-file-list-hook 'ido-sort-mtime)
 (add-hook 'ido-make-dir-list-hook 'ido-sort-mtime)
 
+(defun ido-sort-mtime ()
+  "Sort ido item by modified time."
+  (let (ido-temp-list)
+    (setq ido-temp-list
+          (sort ido-temp-list
+                (lambda (a b)
+                  (let ((ta (nth 5 (file-attributes (concat ido-current-directory a))))
+                        (tb (nth 5 (file-attributes (concat ido-current-directory b)))))
+                    (if (= (nth 0 ta) (nth 0 tb))
+                        (> (nth 1 ta) (nth 1 tb))
+                        (> (nth 0 ta) (nth 0 tb)))))))
+    (ido-to-end ;; move . files to end (again)
+     (delq nil (mapcar
+                (lambda (x) (if (string-equal (substring x 0 1) ".") x))
+                ido-temp-list)))))
+
+
 
 (defun ido-erase-minibuffer-or-dwim ()
   "If cursor the EOL erases whole minibuffer and insert  `~/'.
