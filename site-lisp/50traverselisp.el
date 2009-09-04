3ca091b7663510f753ce2f536b2380c133778d0b
S.P.Tseng <deftsp@gmail.com>
2009-09-04 22:31:02 +0800

    add traverselisp package.

---
+++ site-lisp/50traverselisp.el
@@ -0,0 +1,24 @@
+;;; 50traverselisp.el ---
+
+(require 'traverselisp)
+(require 'anything-traverse)
+
+(setq traverse-use-avfs t)
+
+;; (global-set-key (kbd "<f5> f") 'traverse-deep-rfind)
+
+;; (global-set-key (kbd "<f5> u") 'traverse-build-tags-in-project)
+
+;; (global-set-key (kbd "C-c o") 'traverse-occur-current-buffer)
+
+;; (define-key dired-mode-map (kbd "A")
+;;   'traverse-dired-search-regexp-in-anything-at-point)
+
+;; (define-key dired-mode-map (kbd "C-c C-z")
+;;   'traverse-dired-browse-archive)
+
+;; (define-key dired-mode-map (kbd "C-c t")
+;;   'traverse-dired-find-in-all-files)
+
+;; (add-to-list 'traverse-ignore-files ".ledger-cache")
+
