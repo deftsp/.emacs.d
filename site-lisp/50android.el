0c6f3b1a09e93f7de8fea3d4b72ecac0c19ed7f6
S.P.Tseng <deftsp@gmail.com>
2009-11-17 15:55:07 +0800

    install android-mode.

---
+++ site-lisp/50android.el
@@ -0,0 +1,9 @@
+;;; 50android.el ---
+
+;; Copyright (C) 2009  S.P.Tseng
+
+;; Author: S.P.Tseng <deftsp@gmail.com>
+;; Keywords:
+
+(require 'android-mode)
+(setq android-mode-sdk-dir "~/opt/android-sdk-linux")
