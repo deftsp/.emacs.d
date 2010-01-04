9a9e9655167892c5f4ac01096bbbd18a03e7be51
S.P.Tseng <deftsp@gmail.com>
2010-01-05 02:23:22 +0800

    minor change.

--- site-lisp/50emacsclient.el
+++ site-lisp/50emacsclient.el
@@ -39,6 +39,7 @@
 ;;                 (setq x-select-enable-clipboard t)))))
 
 ;; 2009-09-29 see http://groups.google.com/group/ergoemacs/msg/9eec3b455cab3ff1 and http://stackoverflow.com/questions/885793/emacs-error-when-calling-server-start
-(when (and (= emacs-major-version 23)
-           (equal system-type 'windows-nt))
-  (defun server-ensure-safe-dir (dir) "Noop" t))
+(eval-after-load "server"
+  '(when (and (= emacs-major-version 23)
+          (equal system-type 'windows-nt))
+    (defun server-ensure-safe-dir (dir) "Noop" t)))
