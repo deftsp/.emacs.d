44e068130d803f4192bf3a0f6c66e0d56d70f78d
Shihpin Tseng <deftsp@gmail.com>
2012-03-29 18:09:45 +0800

    ajust pippie-expand-functions-list

--- site-lisp/50completion.el
+++ site-lisp/50completion.el
@@ -64,16 +64,16 @@
 ;; The list of expansion functions tried in order by `hippie-expand'.
 (autoload 'senator-try-expand-semantic "senator")
 (setq hippie-expand-try-functions-list
-      '(try-expand-all-abbrevs             ; Try to expand word before point according to all abbrev tables.
-        try-expand-dabbrev                 ; Try to expand word "dynamically", searching the current buffer.
+      '(senator-try-expand-semantic        ; prefer senatro
+        try-expand-all-abbrevs             ; Try to expand word before point according to all abbrev tables.
         try-expand-dabbrev-visible         ; Try to expand word "dynamically", searching visible window parts.
+        try-expand-dabbrev                 ; Try to expand word "dynamically", searching the current buffer.
         try-expand-dabbrev-all-buffers     ; Tries to expand word "dynamically", searching all other buffers.
         try-expand-dabbrev-from-kill       ; Try to expand word "dynamically", searching the kill ring.
         try-complete-lisp-symbol-partially ; Try to complete as an Emacs Lisp symbol, as many characters as unique.
         try-complete-lisp-symbol           ; Try to complete word as an Emacs Lisp symbol.
-        try-complete-file-name-partially   ; Try to complete text as a file name, as many characters as unique.
         try-complete-file-name             ; Try to complete text as a file name.
-        ;; senator-try-expand-semantic
+        try-complete-file-name-partially   ; Try to complete text as a file name, as many characters as unique.
         try-expand-line                    ; Try to complete the current line to an entire line in the buffer.
         try-expand-line-all-buffers        ; Try to complete the current line, searching all other buffers.
         try-expand-list                    ; Try to complete the current beginning of a list.
