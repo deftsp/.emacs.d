958ad2b839656dc357a9e9020c050e1f3089e0e9
Shihpin Tseng <deftsp@gmail.com>
2012-07-19 14:35:52 +0800

    update workgroup config

--- site-lisp/50workgroups.el
+++ site-lisp/50workgroups.el
@@ -6,14 +6,12 @@
 
 (eval-after-load "workgroups"
   '(progn
-     (setq wg-prefix-key (kbd "C-c w")
-           wg-mode-line-left-brace "["
-           wg-mode-line-right-brace "]"
-           wg-query-for-save-on-emacs-exit t
-           wg-query-for-save-on-workgroups-mode-exit t)
-     (workgroups-mode 1)
-     (setq wg-morph-on nil)
-     (wg-load (expand-file-name' "~/.emacs.d/workgroups"))))
+    (setq wg-prefix-key (kbd "C-c w")
+          wg-mode-line-left-brace "["
+          wg-mode-line-right-brace "]")
+    (workgroups-mode 1)
+    (setq wg-morph-on nil)
+    (wg-load (expand-file-name' "~/.emacs.d/workgroups"))))
 
 
 (provide '50workgroups)
