f002c3a34c3399b22a4c83d05cea0c968ed02e00
Shihpin Tseng <deftsp@gmail.com>
2012-10-19 18:34:44 +0800

    install maxima

--- site-lisp/50maxima.el
+++ site-lisp/50maxima.el
@@ -5,22 +5,33 @@
 ;; Author: Shihpin Tseng <deftsp@gmail.com>
 
 
-;; maxima include imaxima, there's no need to install app-emacs/imaxima
-;; (autoload 'maxima-mode "maxima" "Maxima mode" t)
-;; (autoload 'imaxima "imaxima" "Image support for Maxima." t)
-;; (autoload 'imath-mode "imath" "Interactive Math minor mode." t)
-;; (autoload 'maxima "maxima" "Maxima interaction" t)
+(add-to-list 'load-path "/Applications/Maxima.app/Contents/Resources/maxima/share/maxima/5.28.0/emacs")
+
+(setenv "PATH"
+        (concat
+         "/Applications/Maxima.app/Contents/Resources/maxima/bin:"
+         (getenv "PATH")))
+
+(add-to-list 'exec-path "/Applications/Maxima.app/Contents/Resources/maxima/bin")
+
+
+(autoload 'maxima-mode "maxima" "Maxima mode" t)
+(autoload 'imaxima "imaxima" "Frontend for maxima with Image support" t)
+(autoload 'maxima "maxima" "Maxima interaction" t)
+(autoload 'imath-mode "imath" "Imath mode for math formula input" t)
+
 
 (eval-after-load "imaxima"
-  '(setq imaxima-use-maxima-mode-flag t
-    imaxima-print-tex-command "latex %s; dvips -o imax.ps %s; gv imax.ps"
-    imaxima-linearize-flag t
-    imaxima-max-scale 0.85
-    imaxima-scale-factor 1.0
-    imaxima-fnt-size "normalsize"       ; small | normalsize | large | Large | LARGE | huge | Huge
-    imaxima-pt-size 10                  ; The type size used in LaTeX: 9 | 10 | 11 | 12
-    imaxima-label-color "red"
-    imaxima-equation-color "#b6d3d6"))
+  '(progn
+    (setq imaxima-use-maxima-mode-flag t)
+    (setq imaxima-print-tex-command "latex %s; dvipdf %s.dvi imax.pdf; open imax.pdf") ;"latex %s; dvips -o imax.ps %s; gv imax.ps"
+    (setq imaxima-max-scale 0.85)
+    (setq imaxima-scale-factor 1.0)
+    (setq imaxima-fnt-size "LARGE")     ; small | normalsize | large | Large | LARGE | huge | Huge
+    (setq imaxima-pt-size 11)                     ; The type size used in LaTeX: 9 | 10 | 11 | 12
+    (setq imaxima-label-color "#d0bf8f")
+    (setq imaxima-equation-color "DeepSkyBlue2") ; "#b6d3d6"
+    (setq imaxima-linearize-flag t)))
 
 ;; (setq maxima-info-dir "/usr/share/info/"
 ;;       maxima-use-dynamic-complete t)
@@ -69,9 +80,5 @@
 ;; (add-remove-hook 'maxima-mode-hook 'pl/maxima-mode-hook)
 
 
-
-
 (provide '50maxima)
-
-
 ;;; 50maxima.el ends here
