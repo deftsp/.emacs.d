96abf5833d75578637800b344ce9a054641a3e0b
Shihpin Tseng <deftsp@gmail.com>
2012-10-26 12:40:18 +0800

    add RE Builder "query replace this"

--- site-lisp/50search.el
+++ site-lisp/50search.el
@@ -201,6 +201,26 @@
 
 (define-key isearch-mode-map (kbd "C-o") 'isearch-occur)
 
+;;; query-replace-regexp
+;; http://emacs-journey.blogspot.tw/2012/06/re-builder-query-replace-this.html
+(defun pl/reb-query-replace-this-regxp (replace)
+  "Uses the regexp built with re-builder to query the target buffer.
+This function must be run from within the re-builder buffer, not the target
+buffer.
+
+Argument REPLACE String used to replace the matched strings in the buffer.
+ Subexpression references can be used (\1, \2, etc)."
+  (interactive "sReplace with: ")
+  (if (eq major-mode 'reb-mode)
+      (let ((reg (reb-read-regexp)))
+        (select-window reb-target-window)
+        (save-excursion
+          (beginning-of-buffer)
+          (query-replace-regexp reg replace)))
+      (message "Not in a re-builder buffer!")))
+
+(define-key reb-mode-map "\C-c\M-%" 'pl/reb-query-replace-this-regxp)
+
 
 ;; http://patricklogan.blogspot.com/2008/09/query-replace.html
 ;; qurey-replace, use C-r to
