07b796f1e9a44890f6258b6d89a7e2f2c8c21a47
Shihpin Tseng <deftsp@gmail.com>
2012-12-20 15:49:06 +0800

    install 'elisp-slime-nav'

--- site-lisp/50major-modes.el
+++ site-lisp/50major-modes.el
@@ -162,6 +162,12 @@
 ;; (require 'tooltip-help)
 ;; (define-key emacs-lisp-mode-map (kbd "<f1>") 'th-show-help)
 
+;;; elisp-slime-nav
+;; install with el-get
+;; Elisp go-to-definition with M-. and back again with M-,
+(add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t)))
+(eval-after-load 'elisp-slime-nav '(diminish 'elisp-slime-nav-mode))
+
 
 (eval-after-load "highlight-symbol"
   '(progn
