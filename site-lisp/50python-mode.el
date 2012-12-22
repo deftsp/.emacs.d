edb8cb6cf2b6fe6e98df42c864179f5f810842cd
Shihpin Tseng <deftsp@gmail.com>
2012-12-22 14:25:48 +0800

    fix echo input in interpreter python mode

--- site-lisp/50python-mode.el
+++ site-lisp/50python-mode.el
@@ -6,6 +6,13 @@
 ;; Keywords:
 
 
+(defun pl/init-inferior-python-mode ()
+  ;; do not echo input
+  ;; http://stackoverflow.com/questions/8060609/python-interpreter-in-emacs-repeats-lines
+  (setq comint-process-echoes t))
+
+(add-hook 'inferior-python-mode-hook 'pl/init-inferior-python-mode)
+
 
 (provide '50python-mode)
 ;;; 50python-mode.el ends here
