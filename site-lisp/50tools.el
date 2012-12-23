103f4e587a9fd253c26555554eefe744a0e928ed
Shihpin Tseng <deftsp@gmail.com>
2012-12-23 09:01:20 +0800

    bound 'M-g M-g' to ace-jump-mode.

--- site-lisp/50tools.el
+++ site-lisp/50tools.el
@@ -1008,7 +1008,7 @@ such character is found, following options are shown:
     (ace-jump-mode-enable-mark-sync)
     ;; after enable ace jump mode mark sync, use `C-u C-SPC' is enough
     ;; (define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
-    (define-key global-map (kbd "M-g g") 'ace-jump-mode)))
+    (define-key global-map (kbd "M-g M-g") 'ace-jump-mode)))
 
 
 ;;; jump-char
