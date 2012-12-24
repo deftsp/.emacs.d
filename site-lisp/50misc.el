319ea736db4d6f4783c5366b8ac3a2a4c22ece7d
Shihpin Tseng <deftsp@gmail.com>
2012-12-24 21:14:19 +0800

    enable auto revert on non file buffers

--- site-lisp/50misc.el
+++ site-lisp/50misc.el
@@ -151,6 +151,9 @@
 ;; (that change when you update the config spec), you'll want to make sure you're looking at the most recent version of
 ;; the file.
 (global-auto-revert-mode 1)
+;; Also auto refresh dired, but be quiet about it
+(setq global-auto-revert-non-file-buffers t)
+;; (setq auto-revert-verbose nil)
 
 (eval-after-load "mailcap"
   '(mailcap-parse-mailcaps "~/.mailcap" t))
