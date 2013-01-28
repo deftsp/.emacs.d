0dfae25ea0ae1e8c4f3a922e27b512c0a611fd6d
Shihpin Tseng <deftsp@gmail.com>
2013-01-29 00:16:41 +0800

    add `helm-ghc-browse-document'

--- site-lisp/51helm.el
+++ site-lisp/51helm.el
@@ -10,9 +10,41 @@
 
 
 (require 'helm-config)
-
 ;; (helm-mode 1) ; use ido everywhere instead
 
+;;; helm-command-prefix-key (default to C-x c).
+
+
+;;; ghc-mode
+(defun helm-ghc-browse-document ()
+  "Preconfigured `helm' lightweight version \(buffer -> recentf\)."
+  (interactive)
+  (require 'helm-files)
+  (helm-other-buffer 'helm-c-source-ghc-mod "*helm ghc mod*"))
+
+
+(defvar helm-c-source-ghc-mod
+  '((name . "ghc-browse-document")
+    (init . helm-c-source-ghc-mod)
+    (candidates-in-buffer)
+    (candidate-number-limit . 9999999)
+    (action ("Open" . helm-c-source-ghc-mod-action))))
+
+
+(defun helm-c-source-ghc-mod ()
+  (unless (executable-find "ghc-mod")
+    (error "ghc-mode not find."))
+  (let ((buffer (helm-candidate-buffer 'global)))
+    (with-current-buffer buffer
+      (call-process "ghc-mod" nil t t "list"))))
+
+
+(defun helm-c-source-ghc-mod-action (candidate)
+  (interactive "P")
+  (let* ((pkg (ghc-resolve-package-name candidate)))
+    (helm-aif (and pkg candidate)
+        (ghc-display-document pkg it nil)
+      (message "No document found"))))
 
 
 (provide '51helm)
