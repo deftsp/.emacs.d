6c378261a012f043bf930ba1325afa617af5a1fb
Shihpin Tseng <deftsp@gmail.com>
2013-02-01 23:40:41 +0800

    minor change

--- site-lisp/50haskell.el
+++ site-lisp/50haskell.el
@@ -25,14 +25,6 @@
 ;; (remove-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 
-
-;;"Here comes a short Emacs helper for Haskell coders. It allows you to hoogle
-;;lookup the symbol currently under your cursor." Hoogle is a Haskell search engine.
-;;<http://clemens.endorphin.org/weblog/archives/2007-01.shtml#e2007-01-09T09_57_26.txt>
-;;(require 'hoogle nil t)
-;;(define-key haskell-mode-map [?\C-c ?h] 'hoogle-lookup)
-
-
 ;;; ghc-mod
 ;; install ghc-mod
 ;; % cabal update
