c0bd34d2f32bf682be47506697f05fbc942e26d7
Shihpin Tseng <deftsp@gmail.com>
2013-03-19 01:42:34 +0800

    update ruby config

--- site-lisp/50ruby.el
+++ site-lisp/50ruby.el
@@ -7,7 +7,7 @@
 
 
 ;;; install
-;; install inf-ruby, yaml-mode, rinari with el-get
+;; install yaml-mode, rinari with el-get
 ;; https://github.com/remvee/emacs-rails
 
 ;;; rinari
@@ -28,18 +28,18 @@
 
 ;;; pry
 ;; $ gem install pry
+;; (add-to-list 'inf-ruby-implementations '("pry" . "pry -f"))
+;; (setq inf-ruby-default-implementation "pry")
+;; (setq inf-ruby-first-prompt-pattern "^\\[[0-9]+\\] pry\\((.*)\\)> *")
+;; (setq inf-ruby-prompt-pattern "^\\[[0-9]+\\] pry\\((.*)\\)[>*\"'] *")
 
-(add-to-list 'inf-ruby-implementations '("pry" . "pry -f"))
-(setq inf-ruby-default-implementation "pry")
-(setq inf-ruby-first-prompt-pattern "^\\[[0-9]+\\] pry\\((.*)\\)> *")
-(setq inf-ruby-prompt-pattern "^\\[[0-9]+\\] pry\\((.*)\\)[>*\"'] *")
-
-
+;;; init
 (defun pl/ruby-mode-init ()
   (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
   (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
   (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
   (add-hook 'ruby-mode-hook 'pl/flymake-ruby-enable)
+  (define-key ruby-mode-map "\C-c\C-z" 'pl/ruby-switch-to-inf-dwim)
   (add-to-list 'ac-sources 'ac-source-rsense-method)
   (add-to-list 'ac-sources 'ac-source-rsense-constant))
 
@@ -68,7 +68,11 @@
     ;; (local-set-key (kbd "C-c d") 'flymake-display-err-menu-for-current-line)
     (flymake-mode t)))
 
-
+(defun pl/ruby-switch-to-inf-dwim ()
+  (interactive)
+  (unless inf-ruby-buffer
+    (inf-ruby))
+  (call-interactively 'ruby-switch-to-inf))
 
 
 
