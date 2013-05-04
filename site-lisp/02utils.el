234eb7d9514ed8f16d8e351830c1ebe5fe99695a
Shihpin Tseng <deftsp@gmail.com>
2013-05-04 13:38:56 +0800

    move underline-with-char from "99face.el" to "02utils.el"

--- site-lisp/02utils.el
+++ site-lisp/02utils.el
@@ -24,5 +24,18 @@
           (goto-char (+ end 1)))
       (insert "'"))))
 
+(defun pl/underline-with-char (char)
+  (interactive (list (read-from-minibuffer "Char: ")))
+  (when (= 0 (length char))
+    (error "Need a character"))
+  (setq char (aref char 0))             ; Ignore everything but the first char.
+  (save-excursion
+    (goto-char (point-at-eol))
+    (insert "\n"
+            (make-string (- (point-at-eol)
+                            (point-at-bol))
+                         char))))
+
+
 (provide '02utils)
 ;;; 02utils.el ends here
