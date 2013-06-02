5d4e327ca150394a7db130e0914ac0b8c6e83d88
Shihpin Tseng <deftsp@gmail.com>
2013-06-02 10:18:20 +0800

    update haskell mode config

--- site-lisp/50haskell.el
+++ site-lisp/50haskell.el
@@ -4,36 +4,29 @@
 
 ;; Keywords:
 
-(require 'haskell-mode nil t)
-
-(setq haskell-font-lock-symbols t)
-
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+;; (require 'haskell-mode nil t)
+;; (load "haskell-site-file") ; load by el-get
 
 (defvar pl/haskell-mode-doc-map nil
   "Keymap for documentation commands. Bound to a prefix key.")
 
 (eval-after-load "haskell-mode"
   '(progn
+     (setq haskell-font-lock-symbols t
+           haskell-process-type 'cabal-dev  ; 'cabal-dev
+           haskell-notify-p t)
      (require 'haskell-process)
      (setq haskell-stylish-on-save nil) ; or use M-x haskell-mode-stylish-buffer to call `stylish-haskell'
+     (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
      ;; keymap for documentation
      (setq pl/haskell-mode-doc-map (make-sparse-keymap))
-     (define-key pl/haskell-mode-doc-map (kbd "i") 'inferior-haskell-info)
-     (define-key pl/haskell-mode-doc-map (kbd "t") 'inferior-haskell-type)
+     (define-key pl/haskell-mode-doc-map (kbd "i") 'haskell-process-do-info) ; inferior-haskell-info
+     (define-key pl/haskell-mode-doc-map (kbd "t") 'haskell-process-do-type) ; inferior-haskell-type
      (define-key pl/haskell-mode-doc-map (kbd "a") 'helm-ghc-browse-document)
      (define-key pl/haskell-mode-doc-map (kbd "C-a") 'helm-ghc-browse-document)
      (define-key pl/haskell-mode-doc-map (kbd "h") 'haskell-hoogle)
      (define-key pl/haskell-mode-doc-map (kbd "d") 'inferior-haskell-find-haddock)
-     (define-key pl/haskell-mode-doc-map (kbd "C-d") 'inferior-haskell-find-haddock)
-
-     (define-key haskell-mode-map (kbd "C-c C-d") pl/haskell-mode-doc-map)
-     (define-key haskell-mode-map (kbd "C-M-x") 'inferior-haskell-send-decl)
-     (define-key haskell-mode-map (kbd "C-x C-e") 'inferior-haskell-send-decl)
-     (define-key haskell-mode-map (kbd "C-c |") 'haskell-indent-insert-guard)
-     (define-key haskell-mode-map (kbd "C-j") 'newline)
-     ;; (define-key haskell-mode-map (kbd "C-j") 'haskell-newline-and-indent)
-     (define-key haskell-mode-map (kbd "C-c h") 'haskell-hoogle)))
+     (define-key pl/haskell-mode-doc-map (kbd "C-d") 'inferior-haskell-find-haddock)))
 
 ;;; ghc-mod
 ;; install ghc-mod
@@ -41,18 +34,44 @@
 ;; % cabal install ghc-mod
 (autoload 'ghc-init "ghc" nil t)
 
-
+;;; haskell mode hook
 (add-hook 'haskell-mode-hook 'pl/haskell-mode-setup)
 (defun pl/haskell-mode-setup ()
+  (if (buffer-file-name (current-buffer))
+      (flymake-mode))
+  (ghc-init)
   ;; enable our level computation
   (setq outline-level 'pl/outline-level)
   (outline-minor-mode t)
   ;; initially hide all but the headers
   ;;(hide-body)
+  (define-key haskell-mode-map (kbd "C-c C-d") pl/haskell-mode-doc-map)
+  (define-key haskell-mode-map (kbd "C-M-x") 'inferior-haskell-send-decl)
+  (define-key haskell-mode-map (kbd "C-x C-e") 'inferior-haskell-send-decl)
+  (define-key haskell-mode-map (kbd "C-c |") 'haskell-indent-insert-guard)
+  (define-key haskell-mode-map (kbd "C-j") 'newline)
+  ;; (define-key haskell-mode-map (kbd "C-j") 'haskell-newline-and-indent)
+  (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  ;; Load the current file (and make a session if not already made).
+  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
+  ;; “Bring” the REPL, hiding all other windows apart from the source and the REPL.
+  (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+  ;; Build the Cabal project.
+  (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  ;; Interactively choose the Cabal command to run.
+  (define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
 
-  (if (buffer-file-name (current-buffer))
-      (flymake-mode))
-  (ghc-init))
+  ;; Contextually do clever things on the space key, in particular:
+  ;;   1. Complete imports, letting you choose the module name.
+  ;;   2. Show the type of the symbol after the space.
+  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
+  ;; Jump to the imports. Keep tapping to jump between import groups. C-u C-c I to jump back again.
+  (define-key haskell-mode-map (kbd "C-c I") 'haskell-navigate-imports)
+
+  ;; Jump to the definition of the current symbol.
+  (define-key haskell-mode-map (kbd "M-.") 'haskell-mode-tag-find)
+
+  (define-key haskell-mode-map (kbd "C-c h") 'haskell-hoogle))
 
 ;; this gets called by outline to determine the level. Just use the length of the whitespace
 (defun pl/outline-level ()
@@ -73,12 +92,22 @@
 ;;       haskell-indentation-left-offset 4
 ;;       haskell-indentation-ifte-offset 4)
 
-
 ;;; navigation
 ;; $ cabal install hasktags
 ;; Run M-x haskell-process-generate-tags to generate the TAGS file of the current session directory.
 ;; You can now use M-. on a name in a Haskell buffer which will jump directly to its definition.
-(setq haskell-tags-on-save nil)
+(setq haskell-tags-on-save t)
+
+;;; cabal
+;; Useful to have these keybindings for .cabal files, too.
+(defun pl/haskell-cabal-hook ()
+  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
+  (define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
+  (define-key haskell-cabal-mode-map [?\C-c ?\C-z] 'haskell-interactive-switch))
+
+(add-hook 'haskell-cabal-mode-hook 'pl/haskell-cabal-hook)
+
 
 ;;; hamlet-mode
 (require 'hamlet-mode nil t)
