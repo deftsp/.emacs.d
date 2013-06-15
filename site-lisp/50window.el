e05c147e2133f77a76262c347ae8137fb8164f5f
Shihpin Tseng <deftsp@gmail.com>
2013-06-15 23:31:16 +0800

    bind `C-x !' to `pl/toggle-window-split'

--- site-lisp/50window.el
+++ site-lisp/50window.el
@@ -53,76 +53,35 @@
 ;; (global-set-key (kbd "M-F") 'windmove-right)
 
 
-;;; horizontal <==> vertical
+;;; switch between horizontal and vertical layout of two windows
+;; base on http://whattheemacsd.com/buffer-defuns.el-03.html
+(global-set-key (kbd "C-x !") 'pl/toggle-window-split)
 (defun pl/toggle-window-split ()
-  (interactive)
-  (if (= (count-windows) 2)
-      (let* ((this-win-buffer (window-buffer))
-             (next-win-buffer (window-buffer (next-window)))
-             (this-win-edges (window-edges (selected-window)))
-             (next-win-edges (window-edges (next-window)))
-             (this-win-2nd (not (and (<= (car this-win-edges)
-                                         (car next-win-edges))
-                                     (<= (cadr this-win-edges)
-                                         (cadr next-win-edges)))))
-             (splitter
-              (if (= (car this-win-edges)
-                     (car (window-edges (next-window))))
-                  'split-window-horizontally
-                'split-window-vertically)))
-        (delete-other-windows)
-        (let ((first-win (selected-window)))
-          (funcall splitter)
-          (if this-win-2nd (other-window 1))
-          (set-window-buffer (selected-window) this-win-buffer)
-          (set-window-buffer (next-window) next-win-buffer)
-          (select-window first-win)
-          (if this-win-2nd (other-window 1))))))
-
-;; horizontal-to-vertical
-(defun pl/window-horizontal-to-vertical ()
-  "Switches from a horizontal split to a vertical split."
-  (interactive)
-  (let ((one-buf (window-buffer (selected-window)))
-        (buf-point (point)))
-    (other-window 1)
-    (delete-other-windows)
-    (split-window-horizontally)
-    (switch-to-buffer one-buf)
-    (goto-char buf-point)))
-
-;; vertical-to-horizontal
-(defun pl/window-vertical-to-horizontal ()
-  "Switches from a vertical split to a horizontal split."
-  (interactive)
-  (let ((one-buf (window-buffer (selected-window)))
-        (buf-point (point)))
-    (other-window 1)
-    (delete-other-windows)
-    (split-window-vertically)
-    (switch-to-buffer one-buf)
-    (goto-char buf-point)))
-
-;;; switch two windows
-(global-set-key (kbd "C-x !") 'pl/swap-windows)
-;; someday might want to rotate windows if more than 2 of them
-(defun pl/swap-windows ()
-  "If you have 2 windows, it swaps them."
   (interactive)
   (cond ((not (= (count-windows) 2))
          (message "You need exactly 2 windows to do this."))
         (t
-         (let* ((w1 (first (window-list)))
-                (w2 (second (window-list)))
-                (b1 (window-buffer w1))
-                (b2 (window-buffer w2))
-                (s1 (window-start w1))
-                (s2 (window-start w2)))
-           (set-window-buffer w1 b2)
-           (set-window-buffer w2 b1)
-           (set-window-start w1 s2)
-           (set-window-start w2 s1)))))
-
+         (let* ((this-win-buffer (window-buffer))
+                (next-win-buffer (window-buffer (next-window)))
+                (this-win-edges (window-edges (selected-window)))
+                (next-win-edges (window-edges (next-window)))
+                (this-win-2nd (not (and (<= (car this-win-edges)
+                                            (car next-win-edges))
+                                        (<= (cadr this-win-edges)
+                                            (cadr next-win-edges)))))
+                (splitter
+                 (if (= (car this-win-edges)
+                        (car (window-edges (next-window))))
+                     'split-window-horizontally
+                   'split-window-vertically)))
+           (delete-other-windows)
+           (let ((first-win (selected-window)))
+             (funcall splitter)
+             (if this-win-2nd (other-window 1))
+             (set-window-buffer (selected-window) this-win-buffer)
+             (set-window-buffer (next-window) next-win-buffer)
+             (select-window first-win)
+             (if this-win-2nd (other-window 1)))))))
 
 ;;; for wide-screen display
 ;; http://emacswiki.org/emacs/display-buffer-for-wide-screen.el
