646f6a0f5c0d50f21a3e7acf98426399f96d1722
Shihpin Tseng <deftsp@gmail.com>
2013-08-11 02:06:09 +0800

    update etags config

--- site-lisp/50etags.el
+++ site-lisp/50etags.el
@@ -1,97 +1,62 @@
 ;;; 50etags.el ---
 
-;; M. find a tag(a definition)
-;; C-u M-. find next occurence of tag
-;; M-x vist-tags-table specify a new tag table, update tag table
-;; M-x tags=search regexp search on all files in tags table
-;; M-x tags-query-replace run query-replace on all the files
-;; M=, continue last tags search or query-reaplce
-;; C-M-. find-tag-regexp
-;; C-x 4 . find-tag-other-window
-;; C-x 5 . find-tag-other-frame
+;; ctags and etags are distributed with Emacs
+;; ExuberantCtags is superior to etags in many ways. You can find it here:
+;; http://ctags.sourceforge.net
 
 
-;;----------------------------------------------------------------------------------------------------
-;; etags
-;;----------------------------------------------------------------------------------------------------
+;; M-.                find-tag, with `C-u' find next occurence of tag
+;; M-*                pop-tag-mark
+;; C-M-.              find-tag-regexp
+;; tags-search regexp search on all files in tags table. 'tags-loop-continue ' search for next match
+;; tags-query-replace run query-replace on all the files
+;; M-,                tags-loop-continue. Continue last `tags-search' or `tags-query-replace'.
+;; visit-tags-table   set the `tags-file-name' (global). With prefix arg, set buffer-local instead
+;; C-M-.              find-tag-regexp
+;; C-x 4 .            find-tag-other-window
+;; C-x 5 .            find-tag-other-frame
+;; tags-apropos       list all tags in a tags file that match a regexp
+;; list-tags          list all tags defined in a source file
+;; C-M-i M-<TAB>      completion, using the selected tags table if one is loaded
 
-(require 'etags-table)
-;; (require 'etags-select)
-
-(setq etags-table-search-up-depth 10)
+;;;
+(setq tags-add-tables t) ; always add a new tags table to the current list instead of start a new one
 ;; (setq tags-table-list '("." ".." "../.."))
 
+(require 'etags-table nil t)
+(eval-after-load "etags-table"
+  '(progn
+     (add-to-list  'etags-table-alist `(".*\\.[mh]$" ,(expand-file-name "~/.emacs.d/share/tags/objc.TAGS")))
+     (add-to-list  'etags-table-alist `(".*\\.mm$" ,(expand-file-name "~/.emacs.d/share/tags/objc.TAGS")))
+     (add-to-list  'etags-table-alist `(".*\\.hs$" ,(expand-file-name "~/.emacs.d/share/tags/yesod.TAGS")
+                                        ,(expand-file-name "~/.emacs.d/share/tags/persistent.TAGS")))
+     (setq etags-table-search-up-depth 10)))
 
-(add-to-list  'etags-table-alist '("\\.[mh]$" "~/.emacs.d/share/tags/objc.TAGS"))
-(add-to-list  'etags-table-alist '("\\.mm$" "~/.emacs.d/share/tags/objc.TAGS"))
+(require 'etags-select nil t)
+;; (global-set-key "\M-?" 'etags-select-find-tag-at-point)
+(global-set-key (kbd "M-.") 'etags-select-find-tag)
 
 
-
-;; 一个目录下所有的 *.cpp 和 *.h 文件使用这样的正则表达式 *.[ch]*
-(defun pl/create-tags (dir-name)
-  "Create tags file."
-  (interactive "DDirectory: ")
-  (shell-command
-   (concat "find " dir-name " -type f -name  *.[ch]*  | xargs etags -a ")))
-
-(defun pl/generate-tag-table ()
-  "Generate tag tables under current directory(Linux)."
+;;; generate tags
+(defun pl/create-tags ()
+  "create etag file"
   (interactive)
-  (let
-      ((exp "")
-       (dir ""))
-    (setq dir
-          (read-from-minibuffer "generate tags in: " default-directory)
-          exp
-          (read-from-minibuffer "suffix: "))
+  (let ((suffix (read-from-minibuffer "suffix: "))
+        (dir (read-from-minibuffer "create tags in: " default-directory)))
     (with-temp-buffer
       (shell-command
-       (concat "find " dir " -name \"" exp "\" | xargs etags ")
+       (concat "find " dir " -name \"" suffix "\" | xargs etags -a")
        (buffer-name)))))
 
-(defun pl/create-update-tags ()
-  "Create *.[ch]* and update tags"
-  (interactive)
-  (shell-command
-   (concat "find " default-directory " -name  \"" "*.[ch]*" "\" -type f -exec  etags -a {} \\;"))
-  ;;(visit-tags-table)
-  )
 
+(defun pl/create-haskell-tags (sd dd name)
+  (interactive (list (read-directory-name "source directory: " nil "" t)
+                     (read-directory-name "destination directory: " nil "" t)
+                     (read-from-minibuffer "Tag name(TAGS): " "")))
 
-;; tags-files: M-x tags-files-grep is nice
-;; (autoload 'tags-files-grep "tags-files" "Grep the files in the current tags table for REGEXP." t)
-;; (autoload 'tags-files-list "tags-files" "List all files present in the current tags table." t)
-
-;; (autoload 'goto-last-change "goto-last-change" "Set point to the position of the last change." t)
-
-;;----------------------------------------------------------------------------------------------------
-
-;;; Automatic Tags
-
-;;;  Järneström Jonas  ki.ericsson.se> A smarter
-;;;  find-tag that automagically reruns etags when it cant find a
-;;;  requested item and then makes a new try to locate it.
-;;;  Fri Mar 15 09:52:14 2002
-
-;; (defadvice find-tag (around refresh-etags activate)
-;;   "Rerun etags and reload tags if tag not found and redo find-tag.
-;; If buffer is modified, ask about save before running etags."
-;;   (let ((extension (file-name-extension (buffer-file-name))))
-;;     (condition-case err
-;; 	ad-do-it
-;;       (error (and (buffer-modified-p)
-;; 		  (not (ding))
-;; 		  (y-or-n-p "Buffer is modified, save it? ")
-;; 		  (save-buffer))
-;; 	     (er-refresh-etags extension)
-;; 	     ad-do-it))))
-
-;; (defun er-refresh-etags (&optional extension)
-;;   "Run etags on all peer files in current dir and reload them silently."
-;;   (interactive)
-;;   (shell-command (format "etags *.%s" (or extension "el")))
-;;   (let ((tags-revert-without-query t))	; don't query, revert silently
-;;     (visit-tags-table default-directory nil)))
-
+  (let ((tag-name (if (string= name "") "TAGS" name)))
+   (with-temp-buffer
+     (shell-command
+      (concat "find " sd " -name '*.hs*' | xargs hasktags -e -x -o " dd tag-name)))))
 
 (provide '50etags)
