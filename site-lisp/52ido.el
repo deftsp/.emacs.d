3a0fca49b4249752d565f284248bbd8de730c6a8
Shihpin Tseng <deftsp@gmail.com>
2013-08-12 14:14:05 +0800

    fix select-remote RET from slime-js when ido-ubiquitous-mode on

--- site-lisp/52ido.el
+++ site-lisp/52ido.el
@@ -29,9 +29,12 @@
         (let ((ido-ubiquitous-enable-compatibility nil))
           ad-do-it))))
 
+(ido-ubiquitous-use-new-completing-read slime-js-read-remote-index 'slime-js)
 ;; (ido-ubiquitous-use-new-completing-read webjump 'webjump)
 ;; (ido-ubiquitous-use-new-completing-read yas/expand 'yasnippet)
 ;; (ido-ubiquitous-use-new-completing-read yas/visit-snippet-file 'yasnippet)
+
+
 
 
 (setq ido-enable-prefix nil
