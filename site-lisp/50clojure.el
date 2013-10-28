d256082d14b8985c2845f7dce7e4f9ef3cb740bf
Shihpin Tseng <deftsp@gmail.com>
2013-10-29 06:49:10 +0800

    nrepl was renamed to cider

--- site-lisp/50clojure.el
+++ site-lisp/50clojure.el
@@ -10,7 +10,7 @@
 ;; (require 'clojure-mode)
 ;; (require 'clojure-test-mode) ; Load test support for Clojure
 
-;;; nrepl
+;;; CIDER - a Clojure IDE and REPL for Emacs
 ;; (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
 ;; (setq nrepl-popup-stacktraces nil) ; Stop the error buffer from popping up while working in the REPL buffer:
 
@@ -18,16 +18,16 @@
 
 (eval-after-load "clojure-mode"
   '(progn
-     (if (fboundp 'rainbow-delimiters-mode)
-         (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode))
-     (add-hook 'clojure-mode-hook 'subword-mode)
+     (when (fboundp 'rainbow-delimiters-mode)
+       (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode))
      ;; (add-hook 'clojure-mode-hook 'enable-paredit-mode)
-     (add-hook 'clojure-mode-hook 'nrepl-turn-on-eldoc-mode)))
+     ;; (add-hook 'clojure-mode-hook 'nrepl-turn-on-eldoc-mode)
+     (add-hook 'clojure-mode-hook 'subword-mode)))
 
-(eval-after-load "nrepl"
+(eval-after-load "cider"
   '(progn
-     ;; (add-hook 'nrepl-mode-hook 'enable-paredit-mode)
-     (add-hook 'nrepl-mode-hook 'subword-mode)))
+     ;; (add-hook 'cider-mode-hook 'enable-paredit-mode)
+     (add-hook 'cider-mode-hook 'subword-mode)))
 
 
 (provide '50clojure)
