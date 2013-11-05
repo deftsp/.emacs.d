c8e70b5d762aeb04d28d50d86b8273695f9ed824
Shihpin Tseng <deftsp@gmail.com>
2013-11-05 20:32:11 +0800

    disable anzu

--- site-lisp/50search.el
+++ site-lisp/50search.el
@@ -243,13 +243,14 @@ Argument REPLACE String used to replace the matched strings in the buffer.
 ;; E to edit the replacement string
 
 ;;; anzu -- a minor mode which displays current match and total matches information in the mode-line in various search mode.
-(require 'anzu nil t)
-(eval-after-load "anzu"
-  '(progn
-     (setq anzu-search-threshold 1000)
-     (global-anzu-mode +1)
-     (global-set-key (kbd "M-%") 'anzu-query-replace)
-     (global-set-key (kbd "C-M-%") 'anzu-query-replace-regexp)))
+;;  the information which anzu add to sometimes will not disappeard when search complete.
+;; (require 'anzu nil t)
+;; (eval-after-load "anzu"
+;;   '(progn
+;;      (setq anzu-search-threshold 1000)
+;;      (global-anzu-mode +1)
+;;      (global-set-key (kbd "M-%") 'anzu-query-replace)
+;;      (global-set-key (kbd "C-M-%") 'anzu-query-replace-regexp)))
 
 (provide '50search)
 
