4302a7f769255deab19f887ffeb0066148eafa26
Shihpin Tseng <deftsp@gmail.com>
2013-11-07 18:02:17 +0800

    add custom indentation

--- site-lisp/50clojure.el
+++ site-lisp/50clojure.el
@@ -29,6 +29,11 @@
      ;; (add-hook 'cider-mode-hook 'enable-paredit-mode)
      (add-hook 'cider-mode-hook 'subword-mode)))
 
+;;; Custom indentation
+;; http://jbm.io/2013/11/custom-indentation-in-clojure-mode/
+(put-clojure-indent 'match 1) ; equal to (put 'match 'clojure-indent-function 1)
+(put 'macrolet 'clojure-backtracking-indent '((2) 2))
+
 
 (provide '50clojure)
 ;;; 50clojure.el ends here
