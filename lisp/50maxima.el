3c2a487c7fc35aedc4f142ea71dc794fe65741f9
Shihpin Tseng <deftsp@gmail.com>
2015-02-15 18:52:16 +0800

    clean 50maxima.el.

--- lisp/50maxima.el
+++ lisp/50maxima.el
@@ -4,10 +4,8 @@
 
 ;; Author: Shihpin Tseng <deftsp@gmail.com>
 
-
 (add-to-list 'load-path "/Applications/Maxima.app/Contents/Resources/maxima/share/maxima/5.28.0/emacs")
 (add-to-list 'exec-path "/Applications/Maxima.app/Contents/Resources/maxima/bin")
-
 
 (autoload 'maxima-mode "maxima" "Maxima mode" t)
 (autoload 'imaxima "imaxima" "Frontend for maxima with Image support" t)
@@ -29,49 +27,6 @@
 
 ;; (setq maxima-info-dir "/usr/share/info/"
 ;;       maxima-use-dynamic-complete t)
-
-;; (defun pl/maxima-mode-hook ()
-;;   (setq pl/starting-imaxima nil)
-;;   (defun maxima-start ()
-;;     "Start the Maxima process."
-;;     (interactive)
-;;     (if (not (processp inferior-maxima-process))
-;;         (if (not pl/starting-imaxima)
-;;             (let ((origbuffer (current-buffer)))
-;;               (setq pl/starting-imaxima t)
-;;               (imaxima)
-;;               (setq pl/starting-imaxima nil)
-;;               (switch-to-buffer origbuffer))))
-;;     (if (processp inferior-maxima-process)
-;;         (unless (eq (process-status inferior-maxima-process) 'run)
-;;           (delete-process inferior-maxima-process)
-;;           (save-excursion
-;;             (set-buffer "*maxima*")
-;;             (erase-buffer))
-;;           (setq inferior-maxima-process nil)))
-;;     (unless (processp inferior-maxima-process)
-;;       (setq maxima-input-end 0)
-;;       (let ((mbuf)
-;;             (cmd))
-;;         (if maxima-args
-;;             (setq cmd
-;;                   (append (list 'make-comint "maxima" maxima-command
-;;                                 nil) (split-string maxima-args)))
-;;             (setq cmd (list 'make-comint "maxima" maxima-command)))
-;;         (setq mbuf (eval cmd))
-;;         (save-excursion
-;;           (set-buffer mbuf)
-;;           (setq inferior-maxima-process (get-buffer-process mbuf))
-;;           (if maxima-fix-double-prompt
-;;               (add-to-list 'comint-output-filter-functions
-;;                            'maxima-remove-double-prompt))
-;;           (accept-process-output inferior-maxima-process)
-;;           (while (not (maxima-new-prompt-p))
-;;             (accept-process-output inferior-maxima-process))
-;;           (inferior-maxima-mode)))
-;;       (sit-for 0 maxima-after-output-wait))))
-
-;; (add-remove-hook 'maxima-mode-hook 'pl/maxima-mode-hook)
 
 
 (provide '50maxima)
