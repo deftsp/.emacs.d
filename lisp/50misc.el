9d2f28cfa76c31606e0bea00be8e85ce3aebdea7
Shihpin Tseng <deftsp@gmail.com>
2015-05-23 12:05:23 +0800

    add pl/toggle-letter-case.

--- lisp/50misc.el
+++ lisp/50misc.el
@@ -1409,6 +1409,39 @@ This command is to be used interactively."
 
 ;; (global-set-key (kbd "C-k") 'kill-and-join-forward)
 
+;;; toggle-letter-case
+;; http://ergoemacs.org/emacs/modernization_upcase-word.html
+(defun pl/toggle-letter-case ()
+  "Toggle the letter case of current word or text selection.
+Toggles between: “all lower”, “Init Caps”, “ALL CAPS”."
+  (interactive)
+  (let (p1 p2 (deactivate-mark nil) (case-fold-search nil))
+    (if (region-active-p)
+        (setq p1 (region-beginning) p2 (region-end))
+      (let ((bds (bounds-of-thing-at-point 'word) ) )
+        (setq p1 (car bds) p2 (cdr bds)) ) )
+
+    (when (not (eq last-command this-command))
+      (save-excursion
+        (goto-char p1)
+        (cond
+         ((looking-at "[[:lower:]][[:lower:]]") (put this-command 'state "all lower"))
+         ((looking-at "[[:upper:]][[:upper:]]") (put this-command 'state "all caps") )
+         ((looking-at "[[:upper:]][[:lower:]]") (put this-command 'state "init caps") )
+         ((looking-at "[[:lower:]]") (put this-command 'state "all lower"))
+         ((looking-at "[[:upper:]]") (put this-command 'state "all caps") )
+         (t (put this-command 'state "all lower")))))
+
+    (cond
+     ((string= "all lower" (get this-command 'state))
+      (upcase-initials-region p1 p2) (put this-command 'state "init caps"))
+     ((string= "init caps" (get this-command 'state))
+      (upcase-region p1 p2) (put this-command 'state "all caps"))
+     ((string= "all caps" (get this-command 'state))
+      (downcase-region p1 p2) (put this-command 'state "all lower")) )))
+
+(global-set-key (kbd "M-c") 'pl/toggle-letter-case)
+
 ;;; Coding
 (defun pl/revert-to-gbk-dos ()
   (interactive)
