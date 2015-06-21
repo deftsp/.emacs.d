85b02d4af2e251298ab5303a4460727fb390b7fa
Shihpin Tseng <deftsp@gmail.com>
2015-06-21 09:53:19 +0800

    update 50javascript.el.

--- lisp/50javascript.el
+++ lisp/50javascript.el
@@ -5,13 +5,15 @@
 ;; Author: Shihpin Tseng <deftsp@gmail.com>
 
 ;;; js2-mode
+;; TODO: try skewer-mode https://github.com/skeeto/skewer-mode
+
 ;; (autoload 'js2-mode "js2-mode" nil t)
 ;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
-
 (defun pl/js2-mode-hook ()
   (when (fboundp 'moz-minor-mode)
-   (moz-minor-mode +1)))
+    (define-key js2-mode-map (kbd "C-c C-z") 'run-mozilla)
+    (moz-minor-mode +1)))
 
 (add-hook 'js2-mode-hook 'pl/js2-mode-hook)
 
