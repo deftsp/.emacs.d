b63d4a581ef43da7cffe73635f7215ea4e94f397
Shihpin Tseng <deftsp@gmail.com>
2015-07-05 10:56:15 +0800

    minor change.

--- lisp/50vc.el
+++ lisp/50vc.el
@@ -125,8 +125,7 @@
      (define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
      (setq magit-completing-read-function 'magit-ido-completing-read)))
 
-(setq magit-last-seen-setup-instructions "1.4.0"
-      magit-stage-all-confirm nil
+(setq magit-stage-all-confirm nil
       magit-unstage-all-confirm nil)
 
 
