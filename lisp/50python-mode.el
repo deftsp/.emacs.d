b3b6a2055a11bb310d90d0e51be031f3dc4e3cc7
Shihpin Tseng <deftsp@gmail.com>
2016-08-02 10:07:36 +0800

    python-mode do not use ipython now.

--- lisp/50python-mode.el
+++ lisp/50python-mode.el
@@ -68,14 +68,16 @@
   (setq python-indent-guess-indent-offset nil
         python-indent-offset 4)
 
-  (if (executable-find "ipython")
-      (setq python-shell-interpreter "ipython"
-            python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-            python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-            python-shell-completion-setup-code "from IPython.core.completerlib import module_completion"
-            python-shell-completion-module-string-code "';'.join(module_completion('''%s'''))\n"
-            python-shell-completion-string-code "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
-    (setq python-shell-interpreter "python")))
+  ;; (if (executable-find "ipython")
+  ;;     (setq python-shell-interpreter "ipython"
+  ;;           python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+  ;;           python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+  ;;           python-shell-completion-setup-code "from IPython.core.completerlib import module_completion"
+  ;;           python-shell-completion-module-string-code "';'.join(module_completion('''%s'''))\n"
+  ;;           python-shell-completion-string-code
+  ;;           "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+  ;;   (setq python-shell-interpreter "python"))
+  )
 
 
 (put 'project-venv-name 'safe-local-variable 'stringp)
