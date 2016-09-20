c9c2c0a8fbc14a0ba56967915b7011d96fbb2f2f
Shihpin Tseng <deftsp@gmail.com>
2016-09-20 13:05:39 +0800

    minor change.

--- lisp/13org-mode.el
+++ lisp/13org-mode.el
@@ -191,7 +191,7 @@ to `reorganize-frame', otherwise set to `other-frame'."
     (setq org-agenda-window-setup window-config)
     (message "Update `org-agenda-window-setup' to %s" window-config)))
 
-(paloryemacs/update-org-agenda-window-setup)
+;; (paloryemacs/update-org-agenda-window-setup)
 
 (when window-system
   ;; … ↴, ⬎, ⤷, and ⋱.
