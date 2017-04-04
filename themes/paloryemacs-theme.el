2fc1530c6845524b9178fdc37b17b77c920f6699
Shihpin Tseng <deftsp@gmail.com>
2017-04-04 19:35:41 +0800

    minor change.

--- themes/paloryemacs-theme.el
+++ themes/paloryemacs-theme.el
@@ -284,7 +284,7 @@
    `(popup-menu-selection-face ((t (:background "#0000ff" :foreground ,base03))))
 
    ;; ace-window-mode-line
-   '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
+   '(aw-leading-char-face ((t (:foreground "red" :height 9.0))))
    '(aw-mode-line-face ((t (:foreground "violet" :background unspecified))))
 
    ;; company-mode
