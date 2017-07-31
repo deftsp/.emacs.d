2dca4b627d4f1aed05076bc882a1db2ffc95b905
Shihpin Tseng <deftsp@gmail.com>
2017-07-31 11:49:27 +0800

    minor change.

--- lisp/50key-bindings.el
+++ lisp/50key-bindings.el
@@ -631,9 +631,9 @@ _p_rojectile _s_cratch
 undo-tree-_u_ndo undo-tree-_r_edo
 "
    ("SPC" mark-sexp "mark sexp" :color red)
-   ("+" cfs-increase-fontsize :color red)
-   ("-" cfs-decrease-fontsize :color red)
-   ("=" paloryemacs/cfs-reset-profile-and-fontsize "reset font")
+   ("+" cnfonts-increase-fontsize :color red)
+   ("-" cnfonts-decrease-fontsize :color red)
+   ("=" paloryemacs/cnfonts-reset-profile-and-fontsize "reset font")
    ("a" org-agenda nil)
    ("c" org-capture nil)
    ("g" org-clock-goto nil)
