e269d39b464212fafb27991c6c379e0069df895e
Shihpin Tseng <deftsp@gmail.com>
2017-10-27 04:55:42 +0800

    install org-brain.

--- lisp/13org-mode.el
+++ lisp/13org-mode.el
@@ -1165,6 +1165,21 @@ _h_tml    ^ ^        _A_SCII:
   (evil-org-mode)
   (evil-normalize-keymaps))
 
+
+(use-package org-brain
+  :init
+  (setq org-brain-path (expand-file-name "brain" org-directory))
+  (with-eval-after-load 'evil
+    (evil-set-initial-state 'org-brain-visualize-mode 'emacs))
+  :config
+  (setq org-id-track-globally t)
+  (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
+  (push '("b" "Brain" plain (function org-brain-goto-end)
+          "* %i%?" :empty-lines 1)
+        org-capture-templates)
+  (setq org-brain-visualize-default-choices 'all)
+  (setq org-brain-title-max-length 12))
+
 (with-eval-after-load "org"
   (require 'evil-org)
   (add-hook 'org-mode-hook 'paloryemacs//evil-org-mode)
