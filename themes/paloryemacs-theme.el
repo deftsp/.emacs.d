26ebaa6f6a692ede46d88f6d375b36322f95c4b8
Shihpin Tseng <deftsp@gmail.com>
2017-12-08 13:16:07 +0800

    minor change.

--- themes/paloryemacs-theme.el
+++ themes/paloryemacs-theme.el
@@ -45,7 +45,7 @@
       (orange  "#cb4b16")
       (red     "#dc322f")
       (magenta "#d33682")
-      (violet  "#j6c71c4")
+      (violet  "#6c71c4")
       (blue    "#268bd2")
       (cyan    "#2aa198")
       (green   "#859900")
