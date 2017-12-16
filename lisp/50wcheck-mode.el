37aae1119b58fadf4b0988c9fe8e37b98305dd19
Shihpin Tseng <deftsp@gmail.com>
2017-12-16 23:18:34 +0800

    update wcheck-mode config.

--- lisp/50wcheck-mode.el
+++ lisp/50wcheck-mode.el
@@ -4,27 +4,46 @@
 
 ;; Author: Shihpin Tseng <deftsp@gmail.com>
 
-(defvar paloryemacs/wcheck-mode-map (make-keymap) "wcheck mode key map")
-(define-key paloryemacs/wcheck-mode-map "s" 'wcheck-mode)
-(define-key paloryemacs/wcheck-mode-map "l" 'wcheck-change-language)
-(define-key paloryemacs/wcheck-mode-map "c" 'wcheck-actions)
-(define-key paloryemacs/wcheck-mode-map "n" 'wcheck-jump-forward)
-(define-key paloryemacs/wcheck-mode-map "p" 'wcheck-jump-backward)
+(use-package wcheck-mode
+  :defer t
+  :init
+  (progn
+    (setq-default wcheck-language "Highlight Commentaires")
+    (add-hook 'prog-mode-hook 'wcheck-mode)
+    (defvar paloryemacs/wcheck-mode-map (make-keymap) "wcheck mode key map")
+    (define-key paloryemacs/wcheck-mode-map "s" 'wcheck-mode)
+    (define-key paloryemacs/wcheck-mode-map "l" 'wcheck-change-language)
+    (define-key paloryemacs/wcheck-mode-map "c" 'wcheck-actions)
+    (define-key paloryemacs/wcheck-mode-map "a" 'wcheck-actions)
+    (define-key paloryemacs/wcheck-mode-map "n" 'wcheck-jump-forward)
+    (define-key paloryemacs/wcheck-mode-map "p" 'wcheck-jump-backward)
+    (with-eval-after-load "key-chord"
+      (key-chord-define-global ",w" paloryemacs/wcheck-mode-map))))
 
-
-(if (fboundp 'key-chord-define-global)
-    (key-chord-define-global ",w" paloryemacs/wcheck-mode-map))
-
+(setq wcheck-language-data-defaults
+      '((read-or-skip-faces
+         (latex-mode read
+                     nil ; normal text
+                     font-latex-bold-face
+                     font-latex-italic-face
+                     font-latex-sectioning-0-face
+                     font-latex-sectioning-1-face
+                     font-latex-sectioning-2-face
+                     font-latex-sectioning-3-face
+                     font-latex-sectioning-4-face
+                     font-latex-sectioning-5-face
+                     font-latex-slide-title-face
+                     font-lock-type-face))))
 
 (setq wcheck-language-data
       '(("Highlight Commentaires"
-         (program . paloryemacs/commentaires-keywords-detec)
+         (program . paloryemacs/commentaires-keywords-detect)
          (face . font-lock-warning-face)
          (regexp-start . "\\<")
          (regexp-body . "\\w*\\+?:?")
          (regexp-end . "")
          (read-or-skip-faces
-          ((emacs-lisp-mode c-mode c++-mode objc-mode haskell-mode)
+          ((emacs-lisp-mode c-mode c++-mode objc-mode python-mode lua-mode haskell-mode)
            read font-lock-comment-face font-lock-doc-face)
           (nil)))
         ("Trailing whitespace"
@@ -51,7 +70,7 @@
     "\\<\\(OUTPUT\\)"
     "\\<\\(IMPORTANT\\)"))
 
-(defun paloryemacs/commentaires-keywords-detec (strings)
+(defun paloryemacs/commentaires-keywords-detect (strings)
   (let (found)
     (dolist (string strings found)
       (dolist (regexp paloryemacs/commentaires-keywords)
