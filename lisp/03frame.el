a0da5ab5ab24dde098ea6b935034b9f214e92dd3
Shihpin Tseng <deftsp@gmail.com>
2018-01-30 16:11:51 +0800

    minor change

--- lisp/03frame.el
+++ lisp/03frame.el
@@ -13,17 +13,24 @@
 ;;; set frame title
 ;; it should be set before org-clock loaded, which will use it to set
 ;; `org-frame-title-format-backup'
-(setq frame-title-format
-      (list "GNU Emacs "
-            emacs-version
-            "@" system-name ": "
-            '(:eval
-              (if buffer-file-name
-                  (replace-regexp-in-string
-                   (getenv "HOME") "~"
-                   (file-name-directory buffer-file-name))
-                (buffer-name)))))
-(setq icon-title-format frame-title-format)
+(defun paloryemacs//frame-title-format ()
+  (concat "GNU Emacs "
+          emacs-version
+          "@" (or (file-remote-p default-directory 'host) system-name)
+          ":"
+          (let ((file buffer-file-name))
+            (if file
+                (concat
+                 (when (and (bound-and-true-p projectile-mode)
+                            (projectile-project-p))
+                   (format " [%s]" (projectile-project-name)))
+                 " "
+                 (abbreviate-file-name file))
+              "%b"))))
+
+(when (display-graphic-p)
+  (setq frame-title-format '((:eval (paloryemacs//frame-title-format))))
+  (setq icon-title-format frame-title-format))
 
 
 (provide '03frame)
