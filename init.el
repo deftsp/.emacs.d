5c375d745416e2006667ad3e0133b958b3143628
Shihpin Tseng <deftsp@gmail.com>
2018-06-11 18:49:29 +0800

    minor change

--- init.el
+++ init.el
@@ -101,7 +101,7 @@
 (defconst animate-n-steps 3)
 (defun emacs-reloaded ()
   (animate-string (concat ";; Initialization successful, welcome to "
-                          (substring (emacs-version) 0 16) ".") 0 0)
+                          (substring (emacs-version) 0 14) ".") 0 0)
   (newline-and-indent) (newline-and-indent))
 
 (add-hook 'after-init-hook 'emacs-reloaded)
