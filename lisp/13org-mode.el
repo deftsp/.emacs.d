1b7d280fab0830ac8ae42102b0ce26b9b4d3fa25
Shihpin Tseng <deftsp@gmail.com>
2020-03-02 15:17:55 +0800

    update org-brain config

--- lisp/13org-mode.el
+++ lisp/13org-mode.el
@@ -2196,15 +2196,63 @@ prepended to the element after the #+HEADER: tag."
   (setq org-brain-path (expand-file-name "brain" org-directory))
   (tl/set-leader-keys "aob" 'org-brain-visualize)
   (with-eval-after-load 'evil
-    (evil-set-initial-state 'org-brain-visualize-mode 'emacs))
+    (evil-set-initial-state 'org-brain-visualize-mode 'evilified))
+
+
   :config
+  (with-eval-after-load "evil-evilified-state"
+    (evilified-state-evilify-map org-brain-visualize-mode-map
+      :mode org-brain-visualize-mode
+      :bindings
+      ;; (kbd "C-h") nil
+      "gr" 'revert-buffer
+      "j" 'forward-button
+      "k" 'backward-button
+      "l" 'org-brain-add-resource
+      "h" 'org-brain-add-child-headline
+      "n" 'org-brain-pin
+      "v" 'org-brain-visualize
+      "V" 'org-brain-visualize-follow
+      ))
+
+  ;; ascii-art-to-unicode
+  ;; (use-package ascii-art-to-unicode)
+
+  ;; (defface aa2u-face '((t . nil))
+  ;;   "Face for aa2u box drawing characters")
+
+  ;; (advice-add #'aa2u-1c :filter-return
+  ;;             (lambda (str) (propertize str 'face 'aa2u-face)))
+
+  ;; (defun aa2u-org-brain-buffer ()
+  ;;   (let ((inhibit-read-only t))
+  ;;     (make-local-variable 'face-remapping-alist)
+  ;;     (add-to-list 'face-remapping-alist
+  ;;                  '(aa2u-face . org-brain-wires))
+  ;;     (ignore-errors (aa2u (point-min) (point-max)))))
+
+  ;; (with-eval-after-load 'org-brain
+  ;;   (add-hook 'org-brain-after-visualize-hook #'aa2u-org-brain-buffer))
+
+  (defun tl//org-brain-pretty-wire (old-func &rest arguments)
+    (let ((args arguments))
+      (if (equal args '("V"))
+          (setq args '("▽")))
+      (apply old-func args)))
+
+  (advice-add 'org-brain--insert-wire :around 'tl//org-brain-pretty-wire)
+  ;; (advice-remove 'org-brain--insert-wire 'tl//org-brain-pretty-wire)
+
+
   (setq org-id-track-globally t)
   (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
   (push '("b" "Brain" plain (function org-brain-goto-end)
           "* %i%?" :empty-lines 1)
         org-capture-templates)
-  (setq org-brain-visualize-default-choices 'all)
-  (setq org-brain-title-max-length 12))
+  (setq org-brain-title-max-length 20
+        org-brain-visualize-default-choices 'all
+        org-brain-include-file-entries nil
+        org-brain-file-entries-use-title nil))
 
 ;; https://github.com/kiwanami/emacs-calfw
 (defun tl/calfw-calendar ()
