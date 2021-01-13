cbe1ed391a96383faea4fa19ff426c541405d484
Shihpin Tseng <deftsp@gmail.com>
2021-01-13 16:39:18 +0800

    upgrade smart-jump

--- lisp/50jump.el
+++ lisp/50jump.el
@@ -1,10 +1,8 @@
 ;;; 50jump.el ---                                    -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2019  Shihpin Tseng
+;; Copyright (C) 2021  Shihpin Tseng
 
 ;; Author: Shihpin Tseng <deftsp@gmail.com>
-;; Keywords:
-
 
 ;;; dumb-jump
 (use-package dumb-jump
@@ -20,47 +18,21 @@
     (define-key global-map (kbd "M-g o") 'dumb-jump-go-prefer-external-other-window)))
 
 (use-package smart-jump
-  :commands (smart-jump-go smart-jump-back smart-jump-references smart-jump-peek)
+  :commands (smart-jump-go smart-jump-back smart-jump-references)
   :init
   (general-define-key
-   :states '(normal)
+   :states '(normal visual motion)
    "M-." 'smart-jump-go
    "M-," 'smart-jump-back
-   "M-?" 'smart-jump-references
-   "M-P" ' smart-jump-peek)
+   "M-?" 'smart-jump-references)
   :config
-  (setq smart-jump-default-mode-list
-        '(lisp-mode
-          ;; cc-mode ;; `java-mode', `c-mode', `c++-mode', `objc-mode'
-          clojure-mode
-          elisp-mode
-          elm-mode
-          erlang-mode
-          go-mode
-          haskell-mode
-          (js2-mode rjsx-mode)
-          lispy
-          lua-mode
-          lsp-mode
-          python
-          ruby-mode
-          rust-mode
-          scheme
-          typescript-mode
-          web-mode))
-  (smart-jump-setup-default-registers)
-
-  ;; C/C++
   (smart-jump-register :modes '(c-mode c++-mode)
                        :jump-fn 'ggtags-find-tag-dwim
                        :pop-fn 'ggtags-prev-mark
                        :refs-fn 'ggtags-find-reference
-                       :should-jump  (lambda ()
-                                       (bound-and-true-p ggtags-mode))
+                       :should-jump t
                        :heuristic 'point
-                       :async 3000
-                       :order 1)
-  )
-
+                       :async 500
+                       :order 2))
 
 (provide '50jump)
