a004d3d1161c0441c1bccd1df12ef0e65ecd5e75
Shihpin Tseng <deftsp@gmail.com>
2021-04-28 09:59:36 +0800

    minor change

--- themes/tl-theme.el
+++ themes/tl-theme.el
@@ -508,7 +508,7 @@
    '(flycheck-inline-info ((t (:inherit compilation-info :slant italic))))
 
    ;; flyspell
-   '(flyspell-incorrect ((t (:weight bold :underline (:style wave :color "#00ffff")))))
+   '(flyspell-incorrect ((t (:weight bold :underline (:style wave :color "#ffffff")))))
 
    ;; quick-peek
    '(quick-peek-background-face ((t (:background "#23384d" :height 0.9 :slant italic :extend t))))
