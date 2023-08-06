4461a4b0d0ac34bdfa1d9456f125af8a9bc9e337
Shihpin Tseng <deftsp@gmail.com>
2023-08-06 14:21:03 +0800

    minor change

--- init.el
+++ init.el
@@ -9,13 +9,13 @@
 ;; want to see how often GC happens
 ;; (setq garbage-collection-messages nil)
 
-(setq native-comp-deferred-compilation nil
+(setq native-comp-jit-compilation nil
       native-comp-async-report-warnings-errors 'slient)
 
 (require 'comp)
 (with-eval-after-load 'comp
   ;; https://github.com/raxod502/straight.el/issues/680
-  (add-to-list 'native-comp-deferred-compilation-deny-list "\\(?:[^z-a]*-autoloads\\.el$\\)"))
+  (add-to-list 'native-comp-jit-compilation-deny-list "\\(?:[^z-a]*-autoloads\\.el$\\)"))
 
 ;; always load the newer one between .el and .elc
 ;; https://www.mattduck.com/2021-05-upgrading-to-emacs-28.html
