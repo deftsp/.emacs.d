0d13b50848745d162c448cdc98d7ec20811b88dd
Shihpin Tseng <deftsp@gmail.com>
2025-07-01 21:09:37 +0800

    minor change

--- early-init.el
+++ early-init.el
@@ -29,6 +29,8 @@
 (push '(tool-bar-lines . 0) default-frame-alist)
 ;; (push '(vertical-scroll-bars . right) default-frame-alist)
 
+(tool-bar-mode 0)
+
 ;; Resizing the Emacs frame can be a terribly expensive part of changing the
 ;; font. By inhibiting this, we easily halve startup times with fonts that are
 ;; larger than the system default.
