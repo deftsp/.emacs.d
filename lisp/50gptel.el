162f93fd0cf18c2251af4ac14db357525dd89dc7
Shihpin Tseng <deftsp@gmail.com>
2026-03-02 13:23:36 +0800

    add 50gptel

--- lisp/50gptel.el
+++ lisp/50gptel.el
@@ -8,7 +8,26 @@
 ;;; Code:
 
 (use-package gptel
-  :defer t)
+  :commands (gptel)
+  :config
+  (setq gptel-default-mode 'org-mode)
+
+  (setq gptel-backend
+        (gptel-make-openai "OpenCode-Zen"
+          :host "opencode.ai"
+          :endpoint "/zen/v1/chat/completions"
+          :stream t
+          :key #'gptel-api-key-from-auth-source
+          ;; https://opencode.ai/zen/v1/models
+          :models '(claude-sonnet-4-6
+                    gpt-5.3-codex)))
+
+  (setq gptel-log-level 'debug)
+
+  ;; (setq tl//gptel-model 'gpt-5.3-codex)
+  ;; (setopt gptel-model (or tl//gptel-model (car (gptel-backend-models gptel-backend))))
+
+  (setq gptel-model 'gpt-5.3-codex))
 
 (provide '50gptel)
 ;;; 50gptel.el ends here
