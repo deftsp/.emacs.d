c49babd075402bb1516998333428e4a7e015b75f
Shihpin Tseng <deftsp@gmail.com>
2026-04-20 00:34:12 +0800

    minor change

--- lisp/13org-roam.el
+++ lisp/13org-roam.el
@@ -73,11 +73,11 @@
 
   ;; for org-roam-buffer-toggle
   (add-to-list 'display-buffer-alist
-               '(("\\*org-roam\\*"
-                  (display-buffer-in-direction)
-                  (direction . right)
-                  (window-width . 0.33)
-                  (window-height . fit-window-to-buffer))))
+               '("\\*org-roam\\*"
+                 (display-buffer-in-direction)
+                 (direction . right)
+                 (window-width . 0.33)
+                 (window-height . fit-window-to-buffer)))
 
   (tl/declare-prefix-for-mode 'org-mode "mr" "org-roam")
 
