6634e15a7bcd8811f0af1bb4b1fa1f9ebcb3e1a5
Shihpin Tseng <deftsp@gmail.com>
2026-06-13 13:38:27 +0800

    minor change

--- lisp/50opencode.el
+++ lisp/50opencode.el
@@ -5,11 +5,13 @@
 ;; Author: Shihpin Tseng <deftsp@gmail.com>
 ;; Keywords:
 
+;; https://codeberg.org/sczi/opencode.el
 (use-package opencode
   :straight (opencode :type git :host codeberg :repo "sczi/opencode.el")
   :commands (opencode opencode-connect)
   :bind (:map opencode-session-mode-map
          ("RET" . newline)
+         ("C-c C-c" . comint-send-input)
          ;; change the reasoning/thinking effort (thought level), OpenCode TUI is Ctrl-t
          ("C-c C-t" . opencode-select-variant)
          ("C-c C-v" . opencode-select-model)
